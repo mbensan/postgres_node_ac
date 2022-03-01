82ace6333afc6358934d3a41572b87f00c35caeb
Matias Bensan A <mbensan@gmail.com>
2022-02-28 21:34:46 -0300

    Ejemplo de consultas parametrizadas con y sin objeto JS

--- intro_pg/pool.js
+++ intro_pg/pool.js
@@ -1,21 +1,48 @@
 //import pg from 'pg'
 const { Pool } = require('pg')
 
+// creamos nuestro pool de conexiones
 const pool = new Pool({
   user: 'postgres',
   host: '127.0.0.1',
-  database: 'jeans',
+  database: 'jeanso',
   password: '1005',
   max: 20,
   min: 2,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000
 })
+
 // ejemplo de consulta con Pool
 async function consultar() {
-  const client = await pool.connect()
-  const res = await client.query('select * from ropa')
+  /** 
+   * Primero solicitamos un cliente al pool, en caso de error,
+   * tiramos el error a la consola (con el "catch")
+   */
+  try {
+    const client = await pool.connect()
+  } catch (conn_error) {
+    console.log(conn_error)
+    return
+  }
+  //const res = await client.query('select * from ropa')
+  /**
+   * Después realizamos la consulta
+   */
+  let res;
+  try {
+    res = await client.query({
+      text: 'select * from ropa',
+      rowMode: 'array'
+    })
+  } catch (pg_error) {
+    console.log(pg_error)
+    return;
+  }
   console.log(res.rows)
+  /**
+   * Finalmente liberamos el cliente
+   */
   client.release()
   pool.end()
 }
@@ -24,16 +51,21 @@ async function consultar() {
 // Ejemplo de Query con Strings Parametrizados
 async function ingresar (nombre, color,  talla) {
   const client = await pool.connect()
+  
+  // Ejemplo de consulta con string parametrizado
   const res = await client.query(
     `insert into ropa (nombre, color, talla) values ($1, $2, $3)`,
     [nombre, color, talla]
   ) 
+  
+  // Ejemplo de consulta con string parametrizado y JSON
   const res2 = await client.query({
     text: `insert into ropa (nombre, color, talla) values ($1, $2, $3)`,
     values: [nombre, color, talla]
   })
-  // res y res2 son ambas formas válidas de eecutar una sentencia SQL
+  // res y res2 son ambas formas válidas de ejecutar una sentencia SQL
   client.release()
   pool.end()
 }
-ingresar('Camisa', 'Celeste', 'Skinny')
+//ingresar('Camisa', 'Celeste', 'Skinny')
+consultar()
